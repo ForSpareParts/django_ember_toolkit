88cf5a41a9707fb6057b4033801ec021c70d3f75
David Jackson <altmail@jackson-wallace.net>
2015-08-27 19:35:37 -0500

    Refactored a ton of code into utils.py, added settings

--- django_ember_toolkit/management/commands/create_ember_project.py
+++ django_ember_toolkit/management/commands/create_ember_project.py
@@ -1,50 +1,31 @@
-from os.path import abspath, join
+from os.path import join
 import subprocess
 
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from django.template.loader import render_to_string
-from termcolor import colored
 
-EMBER_PATH = abspath(join(settings.BASE_DIR, 'client'))
-SEPARATOR = '---------------------------------------------------------------'
-
-
-def run_ember_command(cmd_name, *args, **kwargs):
-    command = ['ember', cmd_name] + list(args)
-
-    for key, value in kwargs:
-        command.append('--' + key)
-        command.append("'{}'".format(value))
-
-    print(SEPARATOR)
-    print('Running {}...'.format(colored(' '.join(command), 'green')))
-    print(SEPARATOR)
-
-    subprocess.check_call(command, cwd=EMBER_PATH)
+from . import utils as ember_utils
+from .utils import (
+    SEPARATOR, run_ember_command, assert_required_settings,
+    get_setting, write_initial_config, get_full_ember_path)
 
 
 class Command(BaseCommand):
-    args = 'app_name'
     help = 'Generate a new Ember app for use with your Django project'
 
     def handle(self, *args, **options):
-        if len(args) < 1:
-            raise CommandError("An app_name argument is required.")
-
-        app_name = args[0]
+        assert_required_settings('EMBER_APP_NAME')
 
         print(SEPARATOR)
         print('Generating new Ember project at: ')
-        print(EMBER_PATH)
+        print(get_full_ember_path())
         print(SEPARATOR)
 
         # create the project using ember-cli
-        # subprocess.check_call(
-        #     [
-        #         'ember', 'new', app_name,
-        #         '--dir', EMBER_PATH
-        #     ])
+        subprocess.check_call(
+            [
+                'ember', 'new', get_setting('EMBER_APP_NAME'),
+                '--dir', get_full_ember_path()
+            ])
 
         # install and scaffold the Ember adapter for Django REST Framework
         run_ember_command('install', 'ember-django-adapter')
@@ -53,10 +34,4 @@ class Command(BaseCommand):
         run_ember_command('generate', 'drf-serializer', 'application')
 
         # install the modified config file
-        config_source = render_to_string(
-            'django_ember_toolkit/environment.js',
-            {'app_name': app_name})
-        config_path = join(EMBER_PATH, 'config/environment.js')
-
-        with open(config_path, 'w') as config_file:
-            config_file.write(config_source)
+        write_initial_config()
